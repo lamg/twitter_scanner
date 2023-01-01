820bbfbcd714984765d71cb638bfc3bb823b5118
Luis Ángel Méndez Gort <lamg@protonmail.com>
2023-01-01 21:04:02 +0100

    simplified data population

--- lib/refresh_public_tables.ml
+++ lib/refresh_public_tables.ml
@@ -1,21 +1,12 @@
 open Shared
 
-let delete_authors_by_age ctx =
-  [%rapper execute {sql| DELETE FROM query_authors_by_age |sql}] ()
-  |> do_query ctx.db_ctx.db_uri
-;;
-
-let delete_tweets_by_query ctx =
-  [%rapper execute {sql| DELETE FROM tweets_by_query |sql}] ()
-  |> do_query ctx.db_ctx.db_uri
-;;
-
 let populate_tweets_by_query ctx =
   [%rapper
     execute
       {sql|
-      INSERT INTO tweets_by_query(tweet_query, total)
-      SELECT tweet_query, total FROM tweets_by_query_view |sql}]
+      WITH qt AS (SELECT tweet_query, total FROM tweets_by_query_view)
+      UPDATE tweets_by_query SET total = qt.total
+      FROM qt WHERE tweet_query = qt.tweet_query|sql}]
     ()
   |> do_query ctx.db_ctx.db_uri
 ;;
@@ -24,15 +15,14 @@ let populate_authors_by_age ctx =
   [%rapper
     execute
       {sql|
-      INSERT INTO query_authors_by_age(query, age_in_days)
-      SELECT tweet_query, age FROM query_authors_by_age_view |sql}]
+      WITH qa AS (SELECT tweet_query, age FROM query_authors_by_age_view)
+      UPDATE query_authors_by_age SET age_in_days = qa.age
+      FROM qa WHERE query = qa.tweet_query|sql}]
     ()
   |> do_query ctx.db_ctx.db_uri
 ;;
 
 let main ctx =
-  delete_authors_by_age ctx;
-  delete_tweets_by_query ctx;
-  populate_authors_by_age ctx;
+  let _ = populate_authors_by_age ctx in
   populate_tweets_by_query ctx
 ;;
